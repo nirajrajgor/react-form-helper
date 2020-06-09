52f55eb4e5a4c80cd4488b9efb3e7ac7d0b26273
nirajrajgor <nirajr1992@gmail.com>
2020-06-09 23:23:05 +0530

    Basic form with dynamic binding function onchange added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			formControls: {
+				email: {
+					value: ''
+				},
+				name: {
+					value: ''
+				},
+				password: {
+					value: ''
+				}
+			}
+		}
+	}
+
+	changeHandler = event => {
+		const name = event.target.name;
+		const value = event.target.value;
+
+		this.setState({
+			formControls: {
+				...this.state.formControls,
+				[name]: {
+					...this.state.formControls[name],
+					value
+				}
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h2>Form helper</h2>
+				<form>
+					<label htmlFor="email">Email{' '}</label>
+					<input type="email"
+						name="email"
+						value={this.state.formControls.email.value}
+						onChange={this.changeHandler}
+					/>
+
+					<br /><br />
+					<label htmlFor="name">Name{' '}</label>
+					<input type="text"
+						name="name"
+						value={this.state.formControls.name.value}
+						onChange={this.changeHandler}
+					/>
+
+					<br /><br />
+					<label htmlFor="password">Password{' '}</label>
+					<input type="password"
+						name="password"
+						value={this.state.formControls.password.value}
+						onChange={this.changeHandler}
+					/>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
