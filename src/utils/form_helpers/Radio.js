4d05697c8534d356aedf5be506b3592009ef5eb4
nirajrajgor <nirajr1992@gmail.com>
2020-06-25 21:17:34 +0530

    New input type radio added.

---
+++ src/utils/form_helpers/Radio.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Radio = ({
+	touched,
+	valid,
+	label,
+	value,
+	errorMessage,
+	helpText,
+	options,
+	...props
+}) => {
+	let formControl = "form-control";
+
+	if (touched && !valid) {
+		formControl = "form-control control-error";
+	}
+	return (
+		<div className="form-group">
+			<label htmlFor={props.name}>{label}</label>
+			{
+				options.map(option => (
+					<div className="radio-group" key={option.value}>
+						<input
+							type="radio"
+							className={formControl}
+							id={option.text}
+							selected={value === option.value ? true : null}
+							value={option.value}
+							{...props}
+						/>
+						<label htmlFor={option.text}>{option.text}</label>
+					</div>
+				))
+			}
+
+			{/* Help text or Error */}
+			<div className={errorMessage ? "info-text error-text" : "info-text"}>{errorMessage ? errorMessage : helpText}</div>
+		</div>
+	);
+}
+
+export default Radio;
