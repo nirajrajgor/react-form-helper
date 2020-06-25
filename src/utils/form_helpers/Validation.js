c9f0e51fd81575f009001ce2feb76c8bec0645cf
nirajrajgor <nirajr1992@gmail.com>
2020-06-25 21:18:13 +0530

    Explicitly checked string validations.

--- src/utils/form_helpers/Validation.js
+++ src/utils/form_helpers/Validation.js
@@ -34,7 +34,9 @@ class Validation {
 	static isRequired = value => {
 		if (Array.isArray(value))
 			return value.length !== 0;
-		return value.trim().length !== 0;
+		if (typeof value === 'string')
+			return value.trim().length !== 0;
+		return true;
 	}
 
 	/**
